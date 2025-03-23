04afd04233052c52f57cb931a1feebf5393a971d
asrira428 <asrira428@gmail.com>
2025-03-23 15:54:59 -0400

    Updated Name

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,8 +1,8 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-  firstName: "Selene",
-  lastName: "Yu",
+  firstName: "Anirudh",
+  lastName: "Sriram",
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
