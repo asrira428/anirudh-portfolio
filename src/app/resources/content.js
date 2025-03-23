bfbe2c5e2d2420c40196a473b4e61306cc426c45
asrira428 <asrira428@gmail.com>
2025-03-23 16:29:09 -0400

    fixed date identifier

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -8,7 +8,7 @@ const person = {
   },
   role: "Full Stack Developer",
   avatar: "/images/avatar.jpg",
-  location: "Atlanta, GA", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  location: "America/New_York", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English"], // optional: Leave the array empty if you don't want to display languages
 };
 
